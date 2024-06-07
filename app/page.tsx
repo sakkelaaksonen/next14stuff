58623f85170041d1b91b5705b13f2206bef3b7c1
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2024-06-07 13:48:30 +0300

    link to frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -35,8 +35,8 @@ export default async function Home() {
 
     <div className="mb-0 w-full max-w-5xl text-left">
       <Link
-        href="/demoaction"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        href="/quest"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors underline text-white"
       >
         <h2 className="mb-3 text-2xl font-semibold bg-dark-trans text-white p-8">
           Show me what you got to Quest For
--- components/ActionForm.tsx
+++ components/ActionForm.tsx
@@ -19,57 +19,60 @@ export default function ActionForm({ systemValue }: ActionProps) {
         message: '',
         amount: 0
     }
-    
+
     // This adds prevState to the action parameters
     const [state, formAction] = useFormState(actionWithSystemValue, initialState)
 
- const formClasses = cls('formClasses',{
-    'error-text':state.errors?.text,
-    'error-amount': state.errors?.amount,
-    'error-status': state.errors?.status,
- })
+    const formClasses = cls('formClasses', {
+        'error-text': state.errors?.text,
+        'error-amount': state.errors?.amount,
+        'error-status': state.errors?.status,
+    })
 
-  return (
+    return (
 
-      <>
-      <h1>Server Actions</h1>
-      <div className="block">
-        {/* This adds FormData to the action parameters */}
-        <form action={formAction} className={formClasses}>
-            <label htmlFor="text">What is your Quest?</label>
-            <textarea name="text" id="text-id" ></textarea>
-            <label htmlFor="amount-id">What is your value?</label>
-            <input type="number" name="amount" id="amount-id" />
-            <label htmlFor="status-id">Are you ready?</label>
-            <input type="checkbox" name="status" id="status-id" />
-            <input type="hidden" name="goaway" value="0" />
-            <SubmitButton />
-        </form >
-      </div>
-      {state.errors?.text && <p>Surely you have a purpose for being here.</p>}
-      {state.errors?.amount && <p>Yuu must value yourself more than this in order to succeed.</p>}
-      {state.errors?.status && <p>You are not ready. I shall wait.</p>}
+        <>
 
-      {!state.errors && <ReadyState {...state as ReturnValue} />}
-    </>
-  )
+            <h1>Server Actions</h1>
+            <Link href="/" className="bg-dark-trans text-white underline">See all Quests</Link>
+
+            <div className="block">
+                {/* This adds FormData to the action parameters */}
+                <form action={formAction} className={formClasses}>
+                    <label htmlFor="text">What is your Quest?</label>
+                    <textarea name="text" id="text-id" ></textarea>
+                    <label htmlFor="amount-id">What is your value?</label>
+                    <input type="number" name="amount" id="amount-id" />
+                    <label htmlFor="status-id">Are you ready?</label>
+                    <input type="checkbox" name="status" id="status-id" />
+                    <input type="hidden" name="goaway" value="0" />
+                    <SubmitButton />
+                </form >
+
+            </div>
+            {state.errors?.text && <p>Surely you have a purpose for being here.</p>}
+            {state.errors?.amount && <p>Yuu must value yourself more than this in order to succeed.</p>}
+            {state.errors?.status && <p>You are not ready. I shall wait.</p>}
+
+            {!state.errors && <ReadyState {...state as ReturnValue} />}
+        </>
+    )
 }
 
 const ReadyState = ({ message, amount, text }: ReturnValue) => {
-    
-  if(message === '') {
-    return null
-  }
 
-  const hasMessage = message === 'yes';
-  
-  return (
-      <div className="block text-center">
-          {/* <p>Are you ready? "{message}".   {message == 'no' && ' OK, I shall wait'}</p> */}
-          {hasMessage && text !== '' && <p>What a novel Quest you have! I hope you succeed in {text}</p>}
-          {hasMessage && <p>Twice you are this much: {amount * 2} </p>}
-          <Link href="/" className="bg-dark-trans p-8 m-8 text-white">See all Quests</Link>
-      </div>
-  )
+    if (message === '') {
+        return null
+    }
+
+    const hasMessage = message === 'yes';
+
+    return (
+        <div className="block text-center">
+            {/* <p>Are you ready? "{message}".   {message == 'no' && ' OK, I shall wait'}</p> */}
+            {hasMessage && text !== '' && <p>What a novel Quest you have! I hope you succeed in {text}</p>}
+            {hasMessage && <p>Twice you are this much: {amount * 2} </p>}
+        </div>
+    )
 
 }
